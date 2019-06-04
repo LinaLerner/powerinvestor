ab2d26af90faeca4005b03fff39915c511a80f5d
Elina <45712352+LinaLerner@users.noreply.github.com>
2019-06-04 17:01:16 +0200

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-
-const slider = document.querySelector(".slider");
-const output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
