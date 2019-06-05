3c708c2f7a0e980a0f6c2eee1d25a540b18b5f5c
fabien rizzoli <fabien.rizzoli@gmail.com>
2019-06-05 18:43:07 +0200

    foooooorm

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   resources :projects do
     resources :favorites, only: [:create]
   end
   resources :favorites, only: [:index]
+
+  get 'step1', to: "wizards#step1", as: :step1
+  get 'step2', to: "wizards#step2", as: :step2
+  get 'step3', to: "wizards#step3", as: :step3
 end
+
+
+
