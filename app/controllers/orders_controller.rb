bb2bdde906517d438f5c6c288d3d30c19ba4202d
Merwane HAMADI <merwanehamadi@gmail.com>
2019-06-10 14:55:00 +0200

    tabs dashboard

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def create
     project = Project.find(params[:project_id])
-    @order = Order.create(project: project, amount: params[:amount_cents], state: 'pending', user: current_user)
+    order = Order.create(project: project, amount: params[:amount_cents], state: 'pending', user: current_user)
     respond_to do |format|
       format.html { redirect_to new_order_payment_path(@order) }
       format.js  # <-- will render `app/views/reviews/create.js.erb`
@@ -11,5 +11,7 @@ class OrdersController < ApplicationController
 
   def index
     @orders = Order.where(user: current_user)
+    @orders_in_progress = @orders.select {|order| (order.project.amount_collected.to_f / order.project.amount_needed * 100).round != 100 }
+    @orders_finished = @orders.select {|order| (order.project.amount_collected.to_f / order.project.amount_needed * 100).round == 100 }
   end
 end
