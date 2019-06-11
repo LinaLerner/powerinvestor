0a94b40ee13fdaa2625dfcd893c459d5764de86d
Elina Lerner <elinalerner@gmail.com>
2019-06-11 18:06:13 +0200

    show-v93928

--- app/javascript/plugins/seeMore.js
+++ app/javascript/plugins/seeMore.js
@@ -5,15 +5,18 @@
       const dots = document.getElementById("dots");
       const moreText = document.getElementById("more");
       const btnText = document.getElementById("myBtn");
+      const desc = document.getElementById("description");
 
       if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
+        btnText.innerHTML = '<i class="fas fa-chevron-circle-down"></i>';
         moreText.style.display = "none";
+        desc.style.height = "130px";
       } else {
         dots.style.display = "none";
-        btnText.innerHTML = "Read less";
+        btnText.innerHTML = '<i class="fas fa-chevron-circle-up"></i>';
         moreText.style.display = "inline";
+        desc.style.height = "auto";
       }
     });
   }
