d498d8cb29900ebc5e0987295a475f0c104b0d01
Merwane HAMADI <merwanehamadi@gmail.com>
2019-06-12 15:33:58 +0200

    dashboard-v10

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,9 @@
 class OrdersController < ApplicationController
   def create
-    @project = Project.find(params[:project_id])
+    project = Project.find(params[:project_id])
     @order = Order.create(project: project, amount: params[:amount_cents], state: 'pending', user: current_user)
     redirect_to new_order_payment_path(@order)
+
   end
 
   def index
