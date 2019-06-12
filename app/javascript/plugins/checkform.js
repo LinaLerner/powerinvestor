f38fc8e359603187a2f051165dfc5a5728735d27
fabien rizzoli <fabien.rizzoli@gmail.com>
2019-06-12 18:36:53 +0200

    final form

--- app/javascript/plugins/checkform.js
+++ app/javascript/plugins/checkform.js
@@ -1,10 +1,13 @@
 const manageCheckbox = () => {
   const choices = document.querySelectorAll('.category-chosen-wrapper');
+  const spinner = document.querySelector(".sk-circle");
+  if ( !spinner ) {
   choices.forEach( (choice) => {
     choice.addEventListener('click', (event) => {
         event.currentTarget.classList.toggle("active");
       });
     });
   };
+}
 
 export { manageCheckbox }
