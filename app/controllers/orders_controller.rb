ceb5198dc37c51bc83f3563d3f4ae4841c30c1fa
Merwane HAMADI <merwanehamadi@gmail.com>
2019-06-12 13:50:12 +0200

    improved-dashboard

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,8 @@ class OrdersController < ApplicationController
   end
 
   def index
-    @orders = Order.where(user: current_user)
-    @orders_in_progress = @orders.select {|order| (order.project.amount_collected.to_f / order.project.amount_needed * 100).round != 100 }
+    @orders = Order.where(user: current_user).order('created_at desc')
+    @orders_in_progress = @orders.select {|order| (order.project.amount_collected.to_f / order.project.amount_needed * 100).round != 100 }.sort{|a, b| b.created_at<=>a.created_at}
     @orders_finished = @orders.select {|order| (order.project.amount_collected.to_f / order.project.amount_needed * 100).round == 100 }
   end
 end
