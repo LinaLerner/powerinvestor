732d38740960565111e1ab4fcc16a981e09cd4be
fabien rizzoli <fabien.rizzoli@gmail.com>
2019-06-12 15:56:28 +0200

    form front grid

--- app/javascript/plugins/checkform.js
+++ app/javascript/plugins/checkform.js
@@ -1,5 +1,5 @@
 const manageCheckbox = () => {
-  const choices = document.querySelectorAll('.category-chosen');
+  const choices = document.querySelectorAll('.category-chosen-wrapper');
   choices.forEach( (choice) => {
     choice.addEventListener('click', (event) => {
         event.currentTarget.classList.toggle("active");
