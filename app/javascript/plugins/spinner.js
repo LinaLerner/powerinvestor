9c3bcf16e7336b21dc089c343e7e310e6e5542ed
fabien rizzoli <fabien.rizzoli@gmail.com>
2019-06-12 11:21:52 +0200

    progress bar dynamique

--- app/javascript/plugins/spinner.js
+++ app/javascript/plugins/spinner.js
@@ -2,11 +2,15 @@ const manageSpinner = () => {
   const button = document.querySelector("input.submit")
   const form = document.querySelector(".simple_form.edit_user");
   const spinner = document.querySelector(".sk-circle");
+  const progressBar = document.getElementById("progress-step-4");
+  const message = document.querySelector(".loader-message")
   if ( spinner ) {
     button.addEventListener('click', (event) => {
       event.preventDefault()
       spinner.classList.remove("d-none");
+      message.classList.remove("d-none");
       button.classList.add("d-none");
+      progressBar.classList.add("full");
       setTimeout(() => {
         form.submit();
       }, 2000)
