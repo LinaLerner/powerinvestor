b0d8e70de44b6dae0c6b0decb4f45ac82372ba67
sebastien camusot <scamusot@gmail.com>
2019-06-12 15:40:04 +0200

    filter tag index

--- app/javascript/plugins/init_filter.js
+++ app/javascript/plugins/init_filter.js
@@ -1,9 +1,13 @@
 
 const initFilter = () => {
   const array = Array.from(document.querySelectorAll('.project-category'));
-  array.forEach((checkbox) => {
-    checkbox.addEventListener('click', ()=>{
+  array.forEach((button) => {
+    button.addEventListener('click', ()=>{
     document.querySelector('.button-categories').click();
+    const toto = document.querySelector(`label[for='${event.target.id}']`).classList.toggle('button-tag-clicked');
+    console.log(event.target.id)
+    console.log(toto)
+
     });
   });
 };
